152bf39518a0f2c4c8701adc9b1e1a4c5e7cd0d8
Raison Joseph <raisonjosephau@gmail.com>
2026-03-16 09:02:12 +0530

    Vercel db connection for mongo to improve speed

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,14 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!
+const MONGODB_URI = process.env.hive_MONGODB_URI!;
 
-if (!MONGODB_URI) throw new Error("MONGODB_URI is not defined")
+if (!MONGODB_URI) throw new Error("MONGODB_URI is not defined");
 
-let cached = (global as any).mongoose || { conn: null, promise: null }
-;(global as any).mongoose = cached
+let cached = (global as any).mongoose || { conn: null, promise: null };
+(global as any).mongoose = cached;
 
 export async function connectDB() {
-  if (cached.conn) return cached.conn
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
@@ -16,9 +16,9 @@ export async function connectDB() {
       maxPoolSize: 10,
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
-    })
+    });
   }
 
-  cached.conn = await cached.promise
-  return cached.conn
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
